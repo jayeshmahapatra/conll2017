c374b1cd4080110b2e42bdf0eca03ff7ce37bf70
Miikka Silfverberg <mpsilfve@iki.fi>
2017-05-23 13:37:48 -0600

    Train task 2 models.

---
+++ train_task2_medium.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+PYTHON=python3
+
+mkdir -p task2_models
+
+for l in albanian arabic armenian basque bengali bulgarian catalan czech danish dutch english estonian faroese finnish french georgian german haida hebrew hindi hungarian icelandic irish italian khaling kurmanji latin latvian lithuanian lower-sorbian macedonian navajo northern-sami norwegian-bokmal norwegian-nynorsk persian polish portuguese quechua romanian russian scottish-gaelic serbo-croatian slovak slovene sorani spanish swedish turkish ukrainian urdu welsh; do for i in `seq 1 10`; do $PYTHON scripts/attention_train.py all/task2/"$l"-train-medium all/task2/"$l"-uncovered-dev 10 20 0.03 0.03 task2_models/"$l".medium."$i".model; done; done

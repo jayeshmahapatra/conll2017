d564c3fb9cdb549898abd5e767cda459989d1de7
Miikka Silfverberg <mpsilfve@iki.fi>
2017-05-25 12:07:16 -0600

    Copy symbols added.

--- scripts/attention_train_task2.py
+++ scripts/attention_train_task2.py
@@ -6,7 +6,7 @@ import pickle
 import dynet as dy
 
 from augment import augment
-import attention
+import attention_task2
 
 if __name__=='__main__':
     if len(argv) != 8:
@@ -17,7 +17,7 @@ if __name__=='__main__':
     TRAIN_FN=argv[1]
     DEV_FN=argv[2]
     MAX_SIZE=int(argv[3])
-    attention.EPOCHS=int(argv[4])
+    attention_task2.EPOCHS=int(argv[4])
     ALPHA=float(argv[5])
     BETA=float(argv[6])
     O_FN = argv[7]
@@ -25,7 +25,7 @@ if __name__=='__main__':
     orig_data = [l.strip().split('\t') for l in open(TRAIN_FN).read().split('\n') if l.strip() != '']
     AUG_FACTOR = max(1,int(MAX_SIZE / len(orig_data)))
     data = augment(orig_data,AUG_FACTOR)
-    print(len(data))
+
     stdout.flush()
     idata = [[c for c in lemma] + tags.split(';') for lemma, _, tags in data]
     odata = [[c for c in wf] for _, wf, _ in data]    
@@ -33,13 +33,13 @@ if __name__=='__main__':
     idevdata = [[c for c in lemma] + tags.split(';') for lemma, _, tags in devdata]
     odevdata = [[c for c in wf] for _, wf, _ in devdata]
     
-    characters = set([attention.EOS])
+    characters = set([attention_task2.EOS])
     
-    for wf in idata + odata + idevdata + odevdata:
+    for wf in idata + odata:
         for c in wf:
             characters.add(c)
 
-    attention.init_models(characters, None)
-    attention.train(idata,odata,idevdata,odevdata,ALPHA, BETA, O_FN)
+    attention_task2.init_models(characters, None)
+    attention_task2.train(idata,odata,idevdata,odevdata,ALPHA, BETA, O_FN)
 
 

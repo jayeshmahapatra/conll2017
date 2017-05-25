5be81d95adfab1ba7aa226f9e903027913f32e15
Adam <ajwieme@gmail.com>
2017-05-24 18:21:53 -0600

    only necessary models

--- train_task2_high_adam.sh
+++ train_task2_high_adam.sh
@@ -4,4 +4,4 @@ PYTHON=python3
 
 mkdir -p task2_models
 
-for l in albanian arabic armenian basque bengali bulgarian catalan czech danish dutch english estonian faroese finnish french georgian german haida hebrew hindi hungarian icelandic irish italian khaling kurmanji ; do for i in `seq 1 10`; do $PYTHON scripts/attention_train_task2.py all/task2/"$l"-train-high all/task2/"$l"-uncovered-dev 45000 20 0.03 0.01 task2_models/"$l".high."$i".model; done; done
+for l in armenian basque bengali bulgarian catalan czech danish dutch english estonian faroese finnish french georgian german haida hebrew hindi hungarian icelandic irish italian khaling kurmanji ; do for i in `seq 1 10`; do $PYTHON scripts/attention_train_task2.py all/task2/"$l"-train-high all/task2/"$l"-uncovered-dev 45000 20 0.03 0.01 task2_models/"$l".high."$i".model; done; done
--- train_task2_low_adam.sh
+++ train_task2_low_adam.sh
@@ -4,4 +4,4 @@ PYTHON=python3
 
 mkdir -p task2_models
 
-for l in albanian arabic armenian basque bengali bulgarian catalan czech danish dutch english estonian faroese finnish french georgian german haida hebrew hindi hungarian icelandic irish italian khaling kurmanji; do for i in `seq 1 10`; do $PYTHON scripts/attention_train_task2.py all/task2/"$l"-train-low all/task2/"$l"-uncovered-dev 10000 20 0.03 0.01 task2_models/"$l".low."$i".model; done; done
+for l in basque bengali bulgarian catalan czech danish dutch english estonian faroese finnish french georgian german haida hebrew hindi hungarian icelandic irish italian khaling kurmanji; do for i in `seq 1 10`; do $PYTHON scripts/attention_train_task2.py all/task2/"$l"-train-low all/task2/"$l"-uncovered-dev 10000 20 0.03 0.01 task2_models/"$l".low."$i".model; done; done

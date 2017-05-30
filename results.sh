0924ade81c138d88af51b28530573c7e760cd2f5
Adam <ajwieme@gmail.com>
2017-05-30 14:01:33 -0600

    results

--- results.sh
+++ results.sh
@@ -2,6 +2,6 @@
 
 PYTHON=python3
 
-for l in albanian arabic armenian basque bengali bulgarian catalan czech danish dutch english estonian faroese finnish french georgian german haida hebrew hindi hungarian icelandic irish italian khaling kurmanji; do for i in `seq 1 1`; do
-    $PYTHON scripts/attention_test_task2.py all/task2/"$l"-covered-dev task2_models/"$l".medium."$i".model task2_results/"$l"-test.medium."$i".sys;
+for l in slovak slovene sorani spanish swedish turkish ukrainian urdu welsh; do for i in `seq 1 1`; do
+    $PYTHON scripts/attention_test_task2.py all/task2/"$l"-covered-dev task2_models/"$l".high."$i".model task2_results/"$l"-test.high."$i".sys;
 done; done
